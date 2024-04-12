e094c74cd0ea26aae6d1e2d5e0d2574735e3c9e6
GITHIYON49 <103560812+GITHIYON49@users.noreply.github.com>
2024-04-12 20:50:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let mobileIcon = document.querySelector('.mobile-icon');
+let navLinks = document.querySelector('.nav-links');
+
+
+mobileIcon.addEventListener('click',()=>{
+navLinks.classList.toggle('show');
+})
